4520d839d51fea06cac4807a1bea15fc911d9b1e
4000D <dkdkajej@gmail.com>
2019-05-04 15:24:38 +0900

    update attack.sol

--- contracts/Attack.sol
+++ contracts/Attack.sol
@@ -3,5 +3,9 @@ pragma solidity >=0.4.21 <0.6.0;
 import "./PrivateBank.sol";
 
 contract Attack {
+  PrivateBank public target;
 
+  constructor(address payable _target) public {
+    target = PrivateBank(_target);
+  }
 }
